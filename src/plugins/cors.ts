6bb4d9ba5c59ef67900b836f6f9263471ad53ded
Nitin Rathod <nitin.rathod@nividous.com>
2025-08-25 12:09:29 +0530

    cors frontend website changes added

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,10 +4,10 @@ import cors from '@fastify/cors';
 
 export default fp(async (fastify) => {
   fastify.register(cors, {
-     origin: process.env.NODE_ENV === 'production' 
-      ? "https://trenzo-ui.vercel.app" 
-      : "http://localhost:3000",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true, // Needed for cookies or Authorization headers
+  origin: process.env.NODE_ENV === 'production' 
+    ? "https://trainzo.vercel.app" 
+    : "http://localhost:3000",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // Needed for cookies or Authorization headers
   });
 });
