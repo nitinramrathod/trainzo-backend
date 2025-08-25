4e19c2906597de3a60fedbee55f80ff971f8a4ce
Nitin Rathod <nitin.rathod@nividous.com>
2025-08-25 12:13:16 +0530

    CORS made env based

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,9 +4,7 @@ import cors from '@fastify/cors';
 
 export default fp(async (fastify) => {
   fastify.register(cors, {
-  origin: process.env.NODE_ENV === 'production' 
-    ? "https://trainzo.vercel.app" 
-    : "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true, // Needed for cookies or Authorization headers
   });
