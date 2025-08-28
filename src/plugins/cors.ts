4f2086ed6893556cde615471f678c44aa41f17ed
Nitin Rathod <nitin.rathod@nividous.com>
2025-08-28 13:50:35 +0530

    origin changes added

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,11 +1,22 @@
 import fp from 'fastify-plugin';
 import cors from '@fastify/cors';
 
-
 export default fp(async (fastify) => {
+
+  const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
+
   fastify.register(cors, {
-  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true, // Needed for cookies or Authorization headers
+    origin: (origin, cb) => {
+        // allow REST clients / Postman with no origin
+        if (!origin) return cb(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          cb(null, true);
+        } else {
+          cb(new Error("Not allowed by CORS"), false);
+        }
+      },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true, // Needed for cookies or Authorization headers
   });
 });
